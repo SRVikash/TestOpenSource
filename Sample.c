e1fe0d822a854ae34dd032eefc50cf474b022793
Vikash S R <s.r.vikash.it@gmail.com>
2023-12-16 10:11:14 +0530

    Added Sample C file

---
+++ Sample.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int addNumbers(int a, int b) {
+    int result;
+    return result;
+}
+
+int main() {
+    int num1 = 5, num2 = 10;
+    int sum = addNumbers(num1, num2);
+    printf("Sum: %d\n", sum);
+
+    return 0;
+}
